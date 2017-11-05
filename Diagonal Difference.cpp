3879e65a95c5d2ec8687e570899eda31d480a4a2
Virginia Menezes <alitamenezes@gmail.com>
2017-11-04 21:23:46 -0700

    First commit

---
+++ Diagonal Difference.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int sum_d1=0;   // sum of primary diagonal elements
+    int sum_d2=0;   // sum of secondary diagonal elements
+    int diff=0;
+    vector< vector<int> > a(n,vector<int>(n));
+    for(int a_i = 0;a_i < n;a_i++){
+       for(int a_j = 0;a_j < n;a_j++){
+          cin >> a[a_i][a_j];
+       }
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            if (i==j)
+                sum_d1 += a[i][j];
+            if ((i+j) == (n-1))
+                sum_d2 += a[i][j];
+        }
+    }
+    //cout << sum_d1 << " " << sum_d2 <<endl;
+    diff = sum_d1-sum_d2;
+    cout<< abs(diff);
+    return 0;
+}
+
